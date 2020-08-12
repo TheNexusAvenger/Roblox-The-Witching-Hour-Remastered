36eb9b4f5e19da0b6207992139c80e3fd7ecfd53
TheNexusAvenger <zpcook@googlemail.com>
2020-08-12 13:06:03 -0400

    Added teleport location data.

---
+++ src/ReplicatedStorage/GameData/TeleportLocations.lua
@@ -0,0 +1,18 @@
+--[[
+TheNexusAvenger
+
+Teleportable locations on the map.
+--]]
+
+return {
+    ["Builderman"] = {178,13,0},
+    ["Shedletsky"] = {198,104,0},
+    ["StickMasterLuke"] = {172,177,0},
+    ["Darthskrill"] = {154,77,math.pi/2},
+    ["OstrichSized"] = {106,119,math.pi/2},
+    ["ChiefJustus"] = {61,53,-math.pi/2},
+    ["Sorcus"] = {23,9,math.pi/2},
+    ["SolarCrane"] = {15,88,-math.pi},
+    ["OnlyTwentyCharacters"] = {20,161,math.pi/2},
+    ["Fusroblox"] = {100,193,-math.pi/2},
+}
--- src/StarterPlayerScripts/MapGenerator.client.lua
+++ src/StarterPlayerScripts/MapGenerator.client.lua
@@ -9,10 +9,22 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local RunService = game:GetService("RunService")
 
 local ReplicatedStorageProject = require(ReplicatedStorage:WaitForChild("Project"):WaitForChild("ReplicatedStorage"))
+local TeleportLocations = ReplicatedStorageProject:GetResource("GameData.TeleportLocations")
 local MapGenerator = ReplicatedStorageProject:GetResource("Generation.MapGenerator").new()
 MapGenerator:GenerateCustomCells()
 
 
+
+--Create plates under the teleport locations in case the map doesn't generate in time.
+for LocationName,Location in pairs(TeleportLocations) do
+    local Plate = Instance.new("Part")
+    Plate.Transparency = 1
+    Plate.Anchored = true
+    Plate.Size = Vector3.new(100,1,100)
+    Plate.CFrame = CFrame.new(Location[1] * 100,-0.5,Location[2] * 100) * CFrame.Angles(0,(-math.pi/2) + Location[3],0)
+    Plate.Name = LocationName.."TeleportLocation"
+    Plate.Parent = Workspace
+end
 
 --Set up changing the center to generate.
 local Camera = Workspace.CurrentCamera
