59f2b755903464f0339aafefa520dfccfbc2e69c
TheNexusAvenger <zpcook@googlemail.com>
2020-08-15 01:14:51 -0400

    Added BoolGrid data class for map exploration storage.

---
+++ src/ReplicatedStorage/UI/BoolGrid.lua
@@ -0,0 +1,123 @@
+--[[
+TheNexusAvenger
+
+Data structure for a grid of bools.
+--]]
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local HttpService = game:GetService("HttpService")
+
+local ReplicatedStorageProject = require(ReplicatedStorage:WaitForChild("Project"):WaitForChild("ReplicatedStorage"))
+
+local NexusObject = ReplicatedStorageProject:GetResource("ExternalUtil.NexusInstance.NexusObject")
+
+local BoolGrid = NexusObject:Extend()
+BoolGrid:SetClassName("BoolGrid")
+
+
+
+--[[
+Creates an aspect ratio swticher.
+--]]
+function BoolGrid:__new(GridSizeX,GridSizeY,DefaultValue)
+    self:InitializeSuper()
+
+    --Store the state.
+    self.GridSizeX = GridSizeX
+    self.GridSizeY = GridSizeY
+    self.Grid = {}
+    self.DefaultValue = DefaultValue or false
+
+    --Initialize the grid.
+    for X = 1,GridSizeX do
+        self.Grid[X] = {}
+        for Y = 1,GridSizeY do
+            self.Grid[X][Y] = DefaultValue or false
+        end
+    end
+end
+
+--[[
+Returns the value for a given grid position.
+--]]
+function BoolGrid:GetValue(X,Y)
+    --Return the default if the value is out of range.
+    if X < 1 or X > self.GridSizeX or Y < 1 or Y > self.GridSizeY then
+        return self.DefaultValue
+    end
+
+    --Return the value in the grid.
+    return self.Grid[X][Y]
+end
+
+--[[
+Sets the value for a given grid position.
+--]]
+function BoolGrid:SetValue(X,Y,Value)
+    --Return if the value is out of range.
+    if X < 1 or X > self.GridSizeX or Y < 1 or Y > self.GridSizeY then
+        return
+    end
+
+    --Set the value.
+    self.Grid[X][Y] = Value
+end
+
+--[[
+Initializes the grid from a string.
+--]]
+function BoolGrid:LoadFromString(DataString)
+    --Parse the string.
+    local Data = HttpService:JSONDecode(DataString)
+
+    --Set the data from the string.
+    for X = 1,self.GridSizeX do
+        local Row = Data[X]
+        print(game.HttpService:JSONEncode(Row))
+        local CurrentValue = Row[1]
+        local CurrentNumber = 2
+        local Remaining = Row[2]
+        for Y = 1,self.GridSizeY do
+            --Set the value.
+            self.Grid[X][Y] = CurrentValue
+            Remaining = Remaining - 1
+
+            --Set the next value if there is it should be changed.
+            if Remaining == 0 then
+                CurrentValue = not CurrentValue
+                CurrentNumber = CurrentNumber + 1
+                Remaining = Row[CurrentNumber] or 0
+            end
+        end
+    end
+end
+
+--[[
+Serializes the grid to a string.
+--]]
+function BoolGrid:Serialize()
+    --Serailize the rows.
+    local Data = {}
+    for X,Row in pairs(self.Grid) do
+        local RowData = {Row[1],0}
+        Data[X] = RowData
+        local CurrentValue = Row[1]
+        local CurrentId = 2
+        for Y,Value in pairs(Row) do
+            if Value == CurrentValue then
+                RowData[CurrentId] = RowData[CurrentId] + 1
+            else
+                CurrentId = CurrentId + 1
+                CurrentValue = Value
+                RowData[CurrentId] = 1
+            end
+        end
+    end
+
+    --Return the data as a string.
+    return HttpService:JSONEncode(Data)
+end
+
+
+
+return BoolGrid
---
+++ test/ReplicatedStorage/UI/BoolGridTest.nexusspec.lua
@@ -0,0 +1,87 @@
+--[[
+TheNexusAvenger
+
+Tests the BoolGrid class.
+--]]
+
+local NexusUnitTesting = require("NexusUnitTesting")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local HttpService = game:GetService("HttpService")
+
+local ReplicatedStorageProject = require(ReplicatedStorage:WaitForChild("Project"):WaitForChild("ReplicatedStorage"))
+local BoolGrid = ReplicatedStorageProject:GetResource("UI.BoolGrid")
+
+
+
+--[[
+Setups up the test.
+--]]
+local BoolGridTest = NexusUnitTesting.UnitTest:Extend()
+function BoolGridTest:Setup()
+    --Create the component under testing.
+    self.CuT = BoolGrid.new(5,5)
+end
+
+--[[
+Tests loading a grid from a string.
+--]]
+NexusUnitTesting:RegisterUnitTest(BoolGridTest.new("TestLoadingGrid"):SetRun(function(self)
+    --Load the grid data.
+    self.CuT:LoadFromString(HttpService:JSONEncode({
+        {true,5},
+        {false,5},
+        {true,3,2},
+        {true,1,1,1,1,1},
+        {false,2,1,2}
+    }))
+
+    --Assert the grid is connect.
+    self:AssertEquals(HttpService:JSONEncode({{true,true,true,true,true},{false,false,false,false,false},{true,true,true,false,false},{true,false,true,false,true},{false,false,true,false,false}}),HttpService:JSONEncode(self.CuT.Grid),"Grid is incorrect.")
+end))
+
+--[[
+Tests serializing the grid.
+--]]
+NexusUnitTesting:RegisterUnitTest(BoolGridTest.new("TestSerialize"):SetRun(function(self)
+    --Load the grid data.
+    self.CuT:LoadFromString(HttpService:JSONEncode({
+        {true,5},
+        {false,5},
+        {true,3,2},
+        {true,1,1,1,1,1},
+        {false,2,1,2}
+    }))
+
+    --Assert the data is correct.
+    self:AssertEquals(HttpService:JSONEncode({
+        {true,5},
+        {false,5},
+        {true,3,2},
+        {true,1,1,1,1,1},
+        {false,2,1,2}
+    }),self.CuT:Serialize(),"Data is incorrect.")
+
+    --Assert multiple values are valid and set values.
+    self:AssertEquals(self.CuT:GetValue(-1,1),false,"Out of range value is invalid.")
+    self:AssertEquals(self.CuT:GetValue(1,-1),false,"Out of range value is invalid.")
+    self:AssertEquals(self.CuT:GetValue(1,6),false,"Out of range value is invalid.")
+    self:AssertEquals(self.CuT:GetValue(1,6),false,"Out of range value is invalid.")
+    self:AssertEquals(self.CuT:GetValue(1,1),true,"Value is invalid.")
+    self:AssertEquals(self.CuT:GetValue(2,3),false,"Value is invalid.")
+    self:AssertEquals(self.CuT:GetValue(3,2),true,"Value is invalid.")
+    self.CuT:SetValue(1,3,false)
+    self.CuT:SetValue(5,3,false)
+
+    --Assert the new data is correct.
+    self:AssertEquals(HttpService:JSONEncode({
+        {true,2,1,2},
+        {false,5},
+        {true,3,2},
+        {true,1,1,1,1,1},
+        {false,5}
+    }),self.CuT:Serialize(),"Data is incorrect.")
+end))
+
+
+
+return true
