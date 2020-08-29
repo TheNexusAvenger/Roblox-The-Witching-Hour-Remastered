b7b71091fdb2cf7a5a6e67ea29f2693d5aa8f500
TheNexusAvenger <zpcook@googlemail.com>
2020-08-28 22:16:23 -0400

    Added updating the pet's appearance.

--- src/ServerScriptService/Service/PetService.lua
+++ src/ServerScriptService/Service/PetService.lua
@@ -4,16 +4,29 @@ TheNexusAvenger
 Controls spawning and updating pets.
 --]]
 
+local SLOT_TO_PART_NAME = {
+	PetCostumeHat = "Hat",
+	PetCostumeCollar = "Collar",
+	PetCostumeBack = "Cape",
+	PetCostumeAnkle = "Anklet",
+}
+
+
+
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local ReplicatedStorageProject = require(ReplicatedStorage:WaitForChild("Project"):WaitForChild("ReplicatedStorage"))
 local ServerScriptServiceProject = require(ReplicatedStorage:WaitForChild("Project"):WaitForChild("ServerScriptService")):GetContext(script)
+
+local ItemData = ReplicatedStorageProject:GetResource("GameData.ItemData")
+local PetSkinTextures = ReplicatedStorageProject:GetResource("GameData.PetSkinTextures")
 
 local PetService = ReplicatedStorageProject:GetResource("ExternalUtil.NexusInstance.NexusInstance"):Extend()
 PetService:SetClassName("PetService")
 ServerScriptServiceProject:SetContextResource(PetService)
 
 local PlayerDataService = ServerScriptServiceProject:GetResource("Service.PlayerDataService")
+local InventoryService = ServerScriptServiceProject:GetResource("Service.InventoryService")
 
 
 
@@ -49,7 +62,69 @@ end
 Updates the pet's appearance for a player.
 --]]
 function PetService:UpdatePet(Player)
+    --Return if the character doesn't exist.
+    local Character = Player.Character
+    if not Character then
+        return
+    end
 
+    --Get the pet and return if it doesn't exist.
+    local Pet = Character:FindFirstChild("Pet")
+    if not Pet then
+        return
+    end
+    local PetClothes = Pet:FindFirstChild("Clothes")
+    if not PetClothes then
+        return
+    end
+
+    --Get the pet information.
+    local PetType = PlayerDataService:GetPlayerData(Player):GetValue("CurrentPet")
+    local ClothingItemNames = {}
+    local ClothingItemCostumeName = {}
+    local PartNameToSlot = {}
+    for SlotName,PartName in pairs(SLOT_TO_PART_NAME) do
+        PartNameToSlot[PartName] = SlotName
+        local Item = InventoryService:GetItem(Player,SlotName)
+        if Item then
+            ClothingItemNames[SlotName] = Item.Name
+            if ClothingItemNames[SlotName] then
+                ClothingItemCostumeName[SlotName] = (ItemData[Item.Name] or {}).CostumeName
+            end
+        end
+    end
+
+    --Update the clothes.
+    for _,Part in pairs(PetClothes:GetChildren()) do
+        if Part:IsA("BasePart") and PartNameToSlot[Part.Name] then
+            local Mesh = Part:FindFirstChildWhichIsA("FileMesh")
+            if Mesh then
+                local ClothingItem = ClothingItemNames[PartNameToSlot[Part.Name]]
+                if ClothingItem then
+                    local ClothingItemData = ItemData[ClothingItem]
+                    if ClothingItemData then
+                        Part.Transparency = 0
+                        Mesh.TextureId = "rbxassetid://"..tostring(ClothingItemData.TextureId)
+                    end
+                else
+                    Part.Transparency = 1
+                end
+            end
+        end
+    end
+
+    --Update the pet texture if all 4 parts are part of costume match.
+	local HasFullCostume = (ClothingItemCostumeName["PetCostumeHat"] == ClothingItemCostumeName["PetCostumeCollar"]) and (ClothingItemCostumeName["PetCostumeHat"] == ClothingItemCostumeName["PetCostumeBack"]) and (ClothingItemCostumeName["PetCostumeHat"] == ClothingItemCostumeName["PetCostumeAnkle"])
+	local Costume = HasFullCostume and ClothingItemCostumeName["PetCostumeHat"] or "Default"
+    local PetTextureId = PetSkinTextures[Costume][PetType]
+	for _,Part in pairs(Pet:GetChildren()) do
+		if Part:IsA("BasePart") then
+			local Mesh = Part:FindFirstChildWhichIsA("FileMesh")
+			if Mesh then
+				Mesh.TextureId = "rbxassetid://"..tostring(PetTextureId)
+			end
+		end
+	end
 end
 
 
