68638b4a4c9d07d42984e4b521b5c2567b2429df
TheNexusAvenger <zpcook@googlemail.com>
2020-09-05 11:56:19 -0400

    Added quest satisfy command.

---
+++ src/ServerScriptService/NexusAdminLoader/Commands/SatisfyCurrentQuest.lua
@@ -0,0 +1,62 @@
+--[[
+TheNexusAvenger
+
+Data for a command for giving items to players.
+--]]
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local ReplicatedStorageProject = require(ReplicatedStorage:WaitForChild("Project"):WaitForChild("ReplicatedStorage"))
+local ServerScriptServiceProject = require(ReplicatedStorage:WaitForChild("Project"):WaitForChild("ServerScriptService"))
+local ItemData = ReplicatedStorageProject:GetResource("GameData.ItemData")
+local QuestData = ReplicatedStorageProject:GetResource("GameData.Quest.Quests")
+local QuestService = ServerScriptServiceProject:GetResource("Service.QuestService")
+local InventoryService = ServerScriptServiceProject:GetResource("Service.InventoryService")
+
+
+
+return {
+    Keyword = "satisfycurrentquest",
+    Description = "Satisfies the requirements for a quest. Doesn't complete the quest to allow testing of the dialogs.",
+    Arguments = {
+        {
+            Type = "nexusAdminPlayers",
+            Name = "Players",
+            Description = "Players to satisfy the selected quest.",
+        },
+    },
+    Run = function(self,CommandContext,Players)
+        for _,Player in pairs(Players) do
+            local PlayerQuests = QuestService.PlayerQuests[Player]
+            local SelectedQuestName = PlayerQuests.QuestData.SelectedQuest
+            if SelectedQuestName then
+                local SelectedQuest = QuestData[SelectedQuestName]
+                if SelectedQuest then
+                    if SelectedQuest.QuestType == "Items" then
+                        --Give the remaining items for the quest.
+                        local RequiredItems = SelectedQuest.RequiredItems[1]
+                        for i = 1,RequiredItems.Amount do
+                            InventoryService:AddItem(Player,RequiredItems.Name)
+                        end
+                    elseif SelectedQuest.QuestType == "Dressup" then
+                        --Set the pet item slots to the pet costume.
+                        local RequiredSet = SelectedQuest.RequiredSet.Name
+                        local PlayerInventory = InventoryService.PlayerInventories[Player]
+                        for ItemName,Data in pairs(ItemData) do
+                            if Data.CostumeName == RequiredSet then
+                                PlayerInventory.Inventory[Data.ItemType] = {Name=ItemName}
+                            end
+                        end
+                        PlayerInventory:Save()
+                    elseif SelectedQuest.QuestType == "Monsters" then
+                        --Add monster kills for the quest.
+                        local RequiredMonsters = SelectedQuest.RequiredMonsters[1]
+                        for i = 1,RequiredMonsters.Amount do
+                            PlayerQuests:RegisterMonsterKill(RequiredMonsters.Name)
+                        end
+                    end
+                end
+            end 
+        end
+    end
+}
