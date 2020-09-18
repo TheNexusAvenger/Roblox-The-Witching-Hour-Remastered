804b23a80810e7a3f5cc4ab305d0558e030f7e80
TheNexusAvenger <zpcook@googlemail.com>
2020-09-18 19:49:02 -0400

    Moved player initialization to common function.

--- src/ServerScriptService/GameScript.server.lua
+++ src/ServerScriptService/GameScript.server.lua
@@ -35,33 +35,44 @@ InventoryService:LoadCharacterAssets()
 local NPCService = ServerScriptServiceProject:GetResource("Service.NPCService")
 NPCService:LoadNPCModels()
 
---Set up players joining.
+--Get the services.
 local CharacterService = ServerScriptServiceProject:GetResource("Service.CharacterService")
 local PlayerDataService = ServerScriptServiceProject:GetResource("Service.PlayerDataService")
 local QuestService = ServerScriptServiceProject:GetResource("Service.QuestService")
 local MapDiscoveryService = ServerScriptServiceProject:GetResource("Service.MapDiscoveryService")
 local EnergyService = ServerScriptServiceProject:GetResource("Service.EnergyService")
-Players.PlayerAdded:Connect(function(Player)
+
+
+
+--[[
+Handles a player being added.
+--]]
+local function PlayerAdded(Player)
     PlayerDataService:LoadPlayer(Player)
     QuestService:LoadPlayer(Player)
     InventoryService:LoadPlayer(Player)
     CharacterService:SpawnCharacter(Player)
     MapDiscoveryService:LoadPlayer(Player)
     EnergyService:LoadPlayer(Player)
-end)
-Players.PlayerRemoving:Connect(function(Player)
+end
+
+--[[
+Handles a player being removed.
+--]]
+local function PlayerRemoved(Player)
     MapDiscoveryService:ClearPlayer(Player)
     QuestService:ClearPlayer(Player)
     InventoryService:ClearPlayer(Player)
     PlayerDataService:ClearPlayer(Player)
-end)
+end
+
+
+
+--Set up players joining.
+Players.PlayerAdded:Connect(PlayerAdded)
+Players.PlayerRemoving:Connect(PlayerRemoved)
 for _,Player in pairs(Players:GetPlayers()) do
     coroutine.wrap(function()
-        PlayerDataService:LoadPlayer(Player)
-        QuestService:LoadPlayer(Player)
-        InventoryService:LoadPlayer(Player)
-        CharacterService:SpawnCharacter(Player)
-        MapDiscoveryService:LoadPlayer(Player)
-        EnergyService:LoadPlayer(Player)
+        PlayerAdded(Player)
     end)()
 end
